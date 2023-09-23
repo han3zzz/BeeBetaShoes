3d38132b544f33f84517e77021f0316960770064
HANGOCHAN\HANGOCHAN <hanhnph24141@fpt.edu.vn>
2023-09-23 22:37:40 +0700

    up database

--- src/main/java/com/spring/beebeta/controller/SanPhamController.java
+++ src/main/java/com/spring/beebeta/controller/SanPhamController.java
@@ -3,12 +3,13 @@ package com.spring.beebeta.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
-@RequestMapping("/admin/product")
+@RequestMapping("/admin/products")
 public class SanPhamController {
-    @GetMapping()
+    @GetMapping("/view")
     public String index(){
-        return "admin/sanpham/index.html";
-    }
+            return "admin/sanpham/index.html";
+        }
+
+
 }
