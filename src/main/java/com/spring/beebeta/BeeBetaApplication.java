18bedaeacc3c74c1f4ed606b3116941e5d8d508d
Hân <hanhnph24141@fpt.edu.vn>
2023-09-23 10:58:50 +0700

    Add files via upload

---
+++ src/main/java/com/spring/beebeta/BeeBetaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.beebeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeBetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeBetaApplication.class, args);
+    }
+
+}
