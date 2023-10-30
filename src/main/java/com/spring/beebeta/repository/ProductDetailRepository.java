5729da3d0bede9e50c34dbe7d0fd94a1952f202a
HANGOCHAN\HANGOCHAN <hanhnph24141@fpt.edu.vn>
2023-10-30 21:08:14 +0700

    update

--- src/main/java/com/spring/beebeta/repository/ProductDetailRepository.java
+++ src/main/java/com/spring/beebeta/repository/ProductDetailRepository.java
@@ -28,9 +28,15 @@ public interface ProductDetailRepository extends JpaRepository<ProductDetail,Int
             " join Product pro on pro.Id = e.IdProduct\n" +
             " join Size s on s.Id = p.IdSize \n" +
             " where (CONTAINS(pro.Name, :name) OR CONTAINS(pro.Code, :name) or :name = 'null') and (ma.Id = :idmaterial or :idmaterial is null) and (c.Id = :idcolor or :idcolor is null) and (s.Id = :idsize or :idsize is null) and (e.IdCategory = :idcategory or :idcategory is null) and (e.IdBrand = :idbrand or :idbrand is null) and (e.IdToe = :idtoe or :idtoe is null) " +
-            " and (e.IdSole = :idsole or :idsole is null) and (e.IdShoelace = :idshoelace or :idshoelace is null) and (e.IdHeelcushion = :idheelcushion or :idheelcushion is null) and (e.IdDesign = :iddesign or :iddesign is null) and e.Price >= :min and e.Price <= :max and e.Weight >= :minTL and e.Weight <= :maxTL\n" +
+            " and (e.IdSole = :idsole or :idsole is null) and (e.IdShoelace = :idshoelace or :idshoelace is null) and (e.IdHeelcushion = :idheelcushion or :idheelcushion is null) and (e.IdDesign = :iddesign or :iddesign is null) and e.Price >= :min and e.Price <= :max and e.Weight >= :minTL and e.Weight <= :maxTL and e.Status = 0\n" +
+            " and e.Id in (\n" +
+            "        select IdProductDetail\n" +
+            "        from ProductDetail_Color_Size\n" +
+            "        group by IdProductDetail\n" +
+            "        having SUM(Quantity) < :soLuong AND SUM(Quantity) > :soLuong1 OR :soLuong IS NULL\n" +
+            "    )" +
             " group by e.Id,e.Price,e.Discount,e.Description,e.CreateDate,e.UpdateDate,e.CreateBy,e.UpdateBy,e.Status,e.IdProduct,e.IdBrand,e.IdToe,e.IdShoelace,e.IdCategory,e.IdHeelcushion,e.IdSole,e.IdDesign,e.Weight,e.DiscountDate\n" +
-            " having (SUM(p.Quantity) < :soLuong and SUM(p.Quantity) > :soLuong1  OR :soLuong IS NULL) and e.Status = 0 order by e.createDate desc" +
+            " order by e.createDate desc" +
             " ",nativeQuery = true)
     public List<ProductDetail> getAllByFilter(@Param("name") String name,@Param("idcolor") Integer IdColor , @Param("idsize") Integer IdSize,@Param("idmaterial") Integer IdMaterial,@Param("idcategory") Integer IdCategory , @Param("idbrand") Integer IdBrand ,@Param("idtoe") Integer IdToe,@Param("idsole") Integer IdSole,@Param("idshoelace") Integer IdShoelcae,@Param("idheelcushion") Integer IdHeelcushion,@Param("iddesign") Integer IdDesign,@Param("min") Double min ,@Param("max") Double max,@Param("minTL") Double minTL ,@Param("maxTL") Double maxTL,@Param("soLuong") Integer soLuong,@Param("soLuong1") Integer soLuong1);
     @Query("Select e from ProductDetail  e where e.product.Code = :code")
